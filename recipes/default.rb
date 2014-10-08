0530cca76ff0b43e502bf14711b014f2d1c7c394
Matteo Dessalvi <m.dessalvi@gsi.de>
2014-10-08 16:00:23 +0200

    Basic recipe: install ES and OpenJDK and deploy the config files

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,38 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+%w{openjdk-7-jdk elasticsearch}.each do |pkg|
+   package pkg
+end
+
+# Set some limits for ES:
+template "/etc/security/limits.d/10-elasticsearch.conf" do
+  source "10-elasticsearch.conf.erb"
+  owner 'root' and mode 0755
+  notifies :restart, 'service[elasticsearch]'
+end
+
+# Debian 'default' config:
+template "/etc/default/elasticsearch" do
+  source "elasticsearch.default.erb"
+  owner 'root' and mode 0755
+  notifies :restart, 'service[elasticsearch]'
+end
+
+# ES logging file:
+template "/etc/elasticsearch/logging.yml" do
+   source "logging.yml.erb"
+   notifies :restart, 'service[elasticsearch]'
+end
+
+# ES config file:
+template "/etc/elasticsearch/elasticsearch.yml" do
+  source "elasticsearch.yml.erb"
+   notifies :restart, 'service[elasticsearch]'
+end
+
+service "elasticsearch" do
+  supports :status => true, :restart => true
+  action [ :enable ]
+end
