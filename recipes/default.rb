474464be798d1d69e13dc219d3f110e0ccee5ef1
Matteo Dessalvi <m.dessalvi@gsi.de>
2014-10-09 15:39:30 +0200

    Introduced the PAM module config for ES. Removed owner and permission in some of the configuration files

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,27 +14,32 @@ end
 # Set some limits for ES:
 template "/etc/security/limits.d/10-elasticsearch.conf" do
   source "10-elasticsearch.conf.erb"
-  owner 'root' and mode 0755
   notifies :restart, 'service[elasticsearch]'
+end
+
+# Enable the PAM limits module:
+cookbook_file "elasticsearch.pam" do
+  source "pam/elasticsearch"
+  path "/etc/pam.d/elasticsearch"
+  action :create_if_missing
 end
 
 # Debian 'default' config:
 template "/etc/default/elasticsearch" do
   source "elasticsearch.default.erb"
-  owner 'root' and mode 0755
   notifies :restart, 'service[elasticsearch]'
 end
 
 # ES logging file:
 template "/etc/elasticsearch/logging.yml" do
-   source "logging.yml.erb"
-   notifies :restart, 'service[elasticsearch]'
+  source "logging.yml.erb"
+  notifies :restart, 'service[elasticsearch]'
 end
 
 # ES config file:
 template "/etc/elasticsearch/elasticsearch.yml" do
   source "elasticsearch.yml.erb"
-   notifies :restart, 'service[elasticsearch]'
+  notifies :restart, 'service[elasticsearch]'
 end
 
 service "elasticsearch" do
