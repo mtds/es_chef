7cbbd83b90732592e9068e399519cbf5054b07bc
Matteo Dessalvi <m.dessalvi@gsi.de>
2014-11-18 16:28:59 +0100

    Changed the cookbook name and fixed the description

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'es'
+name             'elasticsearch'
 maintainer       'GSI HPC Team'
 maintainer_email 'hpc@gsi.de'
 license          'All rights reserved'
-description      'Installs/Configures es'
+description      'Installs/Configures Elasticsearch instances'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.4'
