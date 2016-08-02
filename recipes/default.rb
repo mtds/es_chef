a7565145e660bfef5ae892e41c28268db25715a9
Matteo Dessalvi <m.dessalvi@gsi.de>
2016-08-02 12:05:08 +0200

    Fixed rubocop issues on the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,52 +10,52 @@
 [Chef::Recipe, Chef::Resource].each { |l| l.send :include, ::Extensions }
 
 # Java and ES packages:
-%w{openjdk-7-jdk elasticsearch}.each do |pkg|
-   package pkg
+%w(openjdk-7-jdk elasticsearch).each do |pkg|
+  package pkg
 end
 
 # Check if we are using the OpenJDK version 7:
 check_java
 
 # Set some limits for ES:
-template "/etc/security/limits.d/10-elasticsearch.conf" do
-  source "10-elasticsearch.conf.erb"
+template '/etc/security/limits.d/10-elasticsearch.conf' do
+  source '10-elasticsearch.conf.erb'
   notifies :restart, 'service[elasticsearch]'
 end
 
 # Enable the PAM limits module:
-cookbook_file "elasticsearch.pam" do
-  source "pam/elasticsearch"
-  path "/etc/pam.d/elasticsearch"
+cookbook_file 'elasticsearch.pam' do
+  source 'pam/elasticsearch'
+  path '/etc/pam.d/elasticsearch'
   action :create_if_missing
 end
 
 # Debian 'default' config:
-template "/etc/default/elasticsearch" do
-  source "elasticsearch.default.erb"
+template '/etc/default/elasticsearch' do
+  source 'elasticsearch.default.erb'
   notifies :restart, 'service[elasticsearch]'
 end
 
 # ES logging file:
-template "/etc/elasticsearch/logging.yml" do
-  source "logging.yml.erb"
+template '/etc/elasticsearch/logging.yml' do
+  source 'logging.yml.erb'
   notifies :restart, 'service[elasticsearch]'
 end
 
 # ES config file:
-template "/etc/elasticsearch/elasticsearch.yml" do
-  source "elasticsearch.yml.erb"
+template '/etc/elasticsearch/elasticsearch.yml' do
+  source 'elasticsearch.yml.erb'
   notifies :restart, 'service[elasticsearch]'
 end
 
 # ES Plugins:
-if node[:elasticsearch][:plugins_enable] 
-  node[:elasticsearch][:plugins].each do | name, config |
+if node[:elasticsearch][:plugins_enable]
+  node[:elasticsearch][:plugins].each do |name, config|
     install_plugin name, config
   end
 end
 
-service "elasticsearch" do
+service 'elasticsearch' do
   supports :status => true, :restart => true
-  action [ :enable ]
+  action [:enable]
 end
