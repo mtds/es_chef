2c0fea1ac5264173f0d3ba12712320a2cf66161d
Matteo Dessalvi <m.dessalvi@gsi.de>
2017-08-22 15:01:26 +0200

    Updated metadata: new source_url

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,4 @@ license          'All rights reserved'
 description      'Installs/Configures Elasticsearch instances'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.12'
-source_url       'https://gitorious.gsi.de/site-cookbooks/elasticsearch'
+source_url       'https://git.gsi.de/chef/cookbooks/elasticsearch'
